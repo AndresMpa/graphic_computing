bb5482f8af6489390578b91d0ad324517b83bdfb
AndresMpa <andresprieto2703@gmail.com>
2020-04-04 15:26:11 -0500

    Rotting the 3D figure in X

--- clase_12_1.py
+++ clase_12_1.py
@@ -7,6 +7,8 @@ if __name__ == '__main__':
     fps = lib.frames_per_second_basics()
     window = lib.new_window("Mandala", lib.cts.size)
 
+    one_face_1 = [[100, 100], [100, -200], [-100, -200], [-100, 100]]
+
     while run:
         for event in pg.event.get():
             if event.type == pg.QUIT:
@@ -22,5 +24,6 @@ if __name__ == '__main__':
                 if event.button == 5:
                     pass
 
+        lib.polygons(window, one_face_1)
         lib.frames_per_second(fps, 12)
     pg.quit()
