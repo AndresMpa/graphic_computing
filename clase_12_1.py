02d70bbd8f2f42aeb3b43ede10bf3ed0ee35068e
AndresMpa <andresprieto2703@gmail.com>
2020-04-03 19:14:21 -0500

    Extra points for the partial

---
+++ clase_12_1.py
@@ -0,0 +1,15 @@
+import pygame as pg
+import library as lib
+
+if __name__ == '__main__':
+    pg.init()
+    run = True
+    window = lib.new_window("RPG")
+    fps = lib.frames_per_second_basics()
+
+    while run:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                run = False
+        lib.frames_per_second(fps, 12)
+    pg.quit()
