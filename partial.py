dc2ff6a153039a3d092e27b70eb1050cd0ccacbe
AndresMpa <andresprieto2703@gmail.com>
2020-04-08 16:54:36 -0500

    --all

--- partial.py
+++ partial.py
@@ -1,6 +1,6 @@
 import pygame as pg
+import figure as fig
 import library as lib
-import objects as obj
 
 if __name__ == '__main__':
     pg.init()
@@ -8,10 +8,14 @@ if __name__ == '__main__':
     window = lib.new_window("Figure", lib.cts.size)
     fps = lib.frames_per_second_basics()
 
+    figure = lib.screen_into_cartesian_for_array(fig.Up)
+
     while run:
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 run = False
+
+            lib.polygons_filled(window, figure)
 
         lib.frames_per_second(fps, 12)
     pg.quit()
