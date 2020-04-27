685c642a9934254a66174e34da2bfffd8de5751b
AndresMpa <andresprieto2703@gmail.com>
2020-04-27 12:56:59 -0500

    Collides & mapping

--- clase_13.py
+++ clase_13.py
@@ -7,15 +7,21 @@ if __name__ == '__main__':
     run = True
     window = lib.new_window("Environment")
     fps = lib.frames_per_second_basics()
+    background_velocity = [0, 0]
+    background_position = [0, 0]
+    background_information = lib.cts.background.get_rect()
+    background_screen_limits = [lib.cts.width - 50, lib.cts.height - 50]
+    background_limit = [(lib.cts.width - background_information[2]), (lib.cts.height - background_information[3])]
 
     # Groups
     players = pg.sprite.Group()
-    enemies = pg.sprite.Group()
-    shots_players = pg.sprite.Group()
-    shots_enemies = pg.sprite.Group()
+    blocks = pg.sprite.Group()
 
-    player = obj.Player([lib.cts.width / 2 - 35, lib.cts.height - 35])
+    player = obj.Player([lib.cts.width / 2 - 35, lib.cts.height - 70])
     players.add(player)
+
+    block = obj.Block([1300, 300], 300, 200)
+    blocks.add(block)
 
     speed = 5
     direction = 0
@@ -40,14 +46,44 @@ if __name__ == '__main__':
             if event.type == pg.KEYUP:
                 player.velocity = [0, 0]
 
+            player.current_image += 1
+
+        # Control
+        if player.rect.x > background_screen_limits[0]:
+            player.rect.x = background_screen_limits[0]
+            if background_position[0] > background_limit[0]:
+                background_velocity[0] = -5
+            else:
+                background_velocity[0] = 0
+        else:
+            background_velocity[0] = 0
+
+        if player.rect.y > background_screen_limits[1]:
+            player.rect.y = background_screen_limits[1]
+            if background_position[1] > background_limit[1]:
+                background_velocity[1] = -5
+            else:
+                background_velocity[1] = 0
+        else:
+            background_velocity[1] = 0
+
+        for block in blocks:
+            block.block_velocity[0] = background_velocity[0]
+            block.block_velocity[1] = background_velocity[1]
+
         # Updates
         players.update(direction)
+        blocks.update()
 
         # Drawing issues
 
-        lib.fill(window)
+        window.blit(lib.cts.background, background_position)
 
         players.draw(window)
+        blocks.draw(window)
 
         lib.frames_per_second(fps, 2)
+
+        background_position[0] += background_velocity[0]
+        background_position[1] += background_velocity[1]
     pg.quit()
